eba45a1fac2c4e638b8ac558daa8bb09307d9d61
Vu Cao <cdhvu@yahoo.com>
2024-05-02 14:05:09 -0700

    completed exercise 3

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+const NUMBER_OF_LETTERS = 5;
+
 function Guess({ guess }) {
+  const letters = guess ? guess.split("") : Array(NUMBER_OF_LETTERS).fill("");
+
   return (
     <p key={crypto.randomUUID()} className="guess">
-      {guess?.split("").map((letter) => (
+      {letters.map((letter) => (
         <span key={crypto.randomUUID()} className="cell">
           {letter}
         </span>
